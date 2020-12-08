2a8bbe1371b9b87ae4e1469c3ac91846401e4308
mr-U-I1 <75271215+mr-U-I1@users.noreply.github.com>
2020-12-08 08:18:23 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,9 +66,9 @@ def restart(update, context):
 @run_async
 def ping(update, context):
     start_time = int(round(time.time() * 1000))
-    reply = sendMessage("Starting Ping", context.bot, update)
+    reply = sendMessage("<b>Sᴛᴀʀᴛɪɴɢ Pɪɴɢ...</b>", context.bot, update)
     end_time = int(round(time.time() * 1000))
-    editMessage(f'{end_time - start_time} ms', reply)
+    editMessage(f'<b>{end_time - start_time} ᴍs</b>', reply)
 
 
 @run_async
